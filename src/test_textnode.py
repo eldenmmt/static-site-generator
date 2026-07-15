da8f79f93a0debc0643bae4f90ec02eeebf4c296
eldenmmt <eldentrindade@gmail.com>
2026-07-15 12:55:24 +0200

    test: add unit tests for TextNode and create test runner script

---
+++ src/test_textnode.py
@@ -0,0 +1,26 @@
+import unittest
+from textnode import TextNode, TextType
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+
+    def test_eq_false(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.ITALIC)
+        self.assertNotEqual(node, node2)
+
+    def test_eq_false2(self):
+        node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_repr(self):
+        node = TextNode("Test", TextType.TEXT, "https://boot.dev")
+        self.assertEqual(repr(node), "TextNode(Test, text, https://boot.dev)")
+
+if __name__ == "__main__":
+    unittest.main()
