8ef2dd7c194a38ddf62a7f37fe2443f1a084fb41
eldenmmt <eldentrindade@gmail.com>
2026-07-17 12:32:51 +0200

    deploy: build site into docs directory for GitHub Pages

--- src/generate_page.py
+++ src/generate_page.py
@@ -1,7 +1,7 @@
 import os
 from block_markdown import markdown_to_html_node, extract_title
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
     with open(from_path) as content:
@@ -14,17 +14,20 @@ def generate_page(from_path, template_path, dest_path):
     html_string = html_node.to_html()
     title = extract_title(markdown_content)
 
-    final_html = template_content.replace("{{ Title }}", title).replace("{{ Content }}", html_string)
+    final_html = template_content.replace("{{ Title }}", title)
+    final_html = final_html.replace("{{ Content }}", html_string)
+
+    final_html = final_html.replace('href="/', f'href="{basepath}')
+    final_html = final_html.replace('src="/', f'src="{basepath}')
 
     dest_dir = os.path.dirname(dest_path)
-    
     if dest_dir:
         os.makedirs(dest_dir, exist_ok=True)
 
     with open(dest_path, "w", encoding="utf-8") as f:
         f.write(final_html)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     items = os.listdir(dir_path_content)
 
     for item in items:
@@ -35,10 +38,10 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
                 dest_file_name = item.replace(".md", ".html")
                 dest_path = os.path.join(dest_dir_path, dest_file_name)
 
-                generate_page(from_path, template_path, dest_path)
+                generate_page(from_path, template_path, dest_path, basepath)
         
         else:
             new_dest_dir = os.path.join(dest_dir_path, item)
 
-            generate_pages_recursive(from_path, template_path, new_dest_dir)
+            generate_pages_recursive(from_path, template_path, new_dest_dir, basepath)
         
--- src/main.py
+++ src/main.py
@@ -2,9 +2,10 @@ import os
 import shutil
 from copystatic import copy_files_recursive
 from generate_page import generate_page, generate_pages_recursive
+import sys
 
 SOURCE_DIR = "static"
-DEST_DIR = "public"
+DEST_DIR = "docs"
 CONTENT_DIR = "content"
 
 FROM_PATH = "content/index.md"
@@ -14,6 +15,10 @@ DEST_HTML_PATH = "public/index.html"
 def main():
     print("Initiating the site building process...")
 
+    basepath = "/"
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+
     if os.path.exists(DEST_DIR):
         print(f"Deleting the old folder: {DEST_DIR}/")
         shutil.rmtree(DEST_DIR)
@@ -21,7 +26,7 @@ def main():
     print(f"Copying static files from {SOURCE_DIR}/ to {DEST_DIR}/...")
     copy_files_recursive(SOURCE_DIR, DEST_DIR)
 
-    generate_pages_recursive(CONTENT_DIR, TEMPLATE_PATH, DEST_DIR)
+    generate_pages_recursive(CONTENT_DIR, TEMPLATE_PATH, DEST_DIR, basepath)
 
     print("Build completed successfuly!")
     
